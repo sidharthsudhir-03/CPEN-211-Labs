58916f0a76ed295620ef80631a7c9282da969b97
sidharthsudhir-03 <82895862+sidharthsudhir-03@users.noreply.github.com>
2023-11-09 15:34:59 -0800

    Update alu.sv

--- lab 5/alu.sv
+++ lab 5/alu.sv
@@ -1,13 +1,17 @@
 module ALU(Ain,Bin,ALUop,out,Z);
+
+//initializing inputs
 input [15:0] Ain, Bin;
 input [1:0] ALUop;
-output [15:0] out;
-output Z;
-reg [15:0] out;
-reg Z;
+
+//initializing outputs
+output reg[15:0] out;
+output reg Z;
+
 
 always_comb begin 
 
+   //different alu operations to be executed based on ALUop
 	case(ALUop)
 		2'b00: out = Ain + Bin;
 		2'b01: out = Ain - Bin;
@@ -16,6 +20,7 @@ always_comb begin
 		default: out = {16{1'bx}};
 	endcase
 	
+	//determining value of Z depending on output value
 	case(out)
 		{16{1'b0}}: Z = 1'b1;
 		default: Z = 1'b0;
