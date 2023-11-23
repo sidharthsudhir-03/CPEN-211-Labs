99c2e4e9efa7977543c524d8a676ee62e8829eae
sidharthsudhir-03 <82895862+sidharthsudhir-03@users.noreply.github.com>
2023-11-22 17:03:45 -0800

    Update alu.sv

--- lab6/alu.sv
+++ lab6/alu.sv
@@ -5,45 +5,28 @@ module ALU(Ain,Bin,ALUop,out,status);
 	input reg[1:0] ALUop;
 
 	//initializing outputs
-	output [15:0] out;
-	output[2:0] status;
-
-	wire[15:0] AddSub_out;
+	output reg [15:0] out;
+	output reg[2:0] status;
 	
-	AddSub #(16) (Ain, Bin, ALUop[0], AddSub_out, status[1]);
-	assign out = (ALUop[1] == 1'b0) ? AddSub_out : ((ALUop[0] == 1'b0) ? Ain & Bin : ~Bin);
-	assign {status[2], status[0]} = {(out[15] == {1'b1}) ? 1'b1 : 1'b0 , (out == {16{1'b0}}) ? 1'b1 : 1'b0};		
+	wire [16:0] ovf_out;
+	assign ovf_out = Ain - Bin;
+
+always_comb begin 
+
+   //different alu operations to be executed based on ALUop
+	case(ALUop)
+		2'b00: {out, status} = {Ain + Bin, 3'bxxx};
+		2'b01: {out, status} = {{16{1'bx}}, (ovf_out[15] == 1'b1) ? 1'b1 : 1'b0, (ovf_out[16] == 1'b1) ? 1'b1 : 1'b0, (ovf_out[15:0] == {16{1'b0}}) ? 1'b1 : 1'b0} ;
+		2'b10: {out, status} = {Ain & Bin, 3'bxxx};
+		2'b11: {out, status} = {~Bin, 3'bxxx};	
+		default: out = {16{1'bx}};
+	endcase
+	
+	
+end		
 
 endmodule 
 
-// add a+b or subtract a-b, check for overflow
-module AddSub(a,b,sub,s,ovf) ;
-  parameter n = 8 ;
-  input [n-1:0] a, b ;
-  input sub ;           // subtract if sub=1, otherwise add
-  output [n-1:0] s ;
-  output ovf ;          // 1 if overflow
-  wire c1, c2 ;         // carry out of last two bits
-  wire ovf = c1 ^ c2 ;  // overflow if signs don't match
 
-  // add non sign bits
-  Adder #(n-1) ai(a[n-2:0],b[n-2:0]^{n-1{sub}},sub,c1,s[n-2:0]) ;
-  // add sign bits
-  Adder #(1) as(a[n-1],b[n-1]^sub,c1,c2,s[n-1]) ;
-endmodule
-
-
-// multi-bit adder - behavioral
-module Adder(a,b,cin,cout,s) ;
-  parameter n = 8 ;
-  input [n-1:0] a, b ;
-  input cin ;
-  output [n-1:0] s ;
-  output cout ;
-  wire [n-1:0] s;
-  wire cout ;
-
-  assign {cout, s} = a + b + cin ;
-endmodule 
 
 
