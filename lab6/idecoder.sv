cf483eeb6f4e674b1c9d441ee5dbf9dd4ba746fb
sidharthsudhir-03 <82895862+sidharthsudhir-03@users.noreply.github.com>
2023-11-22 22:42:58 -0800

    Update idecoder.sv

--- lab6/idecoder.sv
+++ lab6/idecoder.sv
@@ -1,11 +1,12 @@
 module idecoder(instruction, nsel, ALUop, sximm5, sximm8, shift, readnum, writenum, opcode, op);
 
 	input[15:0] instruction;
-	input[1:0] nsel;
+	input[2:0] nsel;
 	output[15:0] sximm5, sximm8;
 	output[1:0] ALUop, shift, op;
-	output[2:0] readnum, writenum, opcode;
-	wire[3:0] Rn, Rd, Rm;
+	output[2:0] writenum, opcode;
+	wire[2:0] Rn, Rd, Rm;
+	output reg[2:0] readnum;
 	
 	assign sximm5 = {{11{instruction[4]}}, instruction[4:0]};
 	assign sximm8 = {{8{instruction[7]}}, instruction[7:0]};
@@ -16,6 +17,22 @@ module idecoder(instruction, nsel, ALUop, sximm5, sximm8, shift, readnum, writen
 	assign Rn = instruction[10:8];
 	assign Rd = instruction[7:5];
 	assign Rm = instruction[2:0];
-	assign readnum = (nsel[1] == 1'b0) ? ((nsel[0] == 1'b0) ? Rn : Rd) : ((nsel[0] == 1'b0) ? Rm : 3'bxxx);
+	
+	always_comb begin 
+	
+		case(nsel)
+			
+			3'b001: readnum = Rn;
+			3'b010: readnum = Rd;
+			3'b100: readnum = Rm;
+			default: readnum = 3'bxxx;
+			
+		endcase
+		
+	end
+	
+	assign writenum = readnum;
+	
+	
 	
 endmodule 
