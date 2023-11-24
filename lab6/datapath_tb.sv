e2102230f24348fb0e17425d31d21688eaa4dc8a
sidharthsudhir-03 <82895862+sidharthsudhir-03@users.noreply.github.com>
2023-11-23 19:37:43 -0800

    Update datapath_tb.sv

--- lab6/datapath_tb.sv
+++ lab6/datapath_tb.sv
@@ -2,13 +2,15 @@ module datapath_tb;
   
 
   reg clk;
-  reg [15:0] datapath_in;
-  reg write, vsel, loada, loadb, asel, bsel, loadc, loads;
+  reg [15:0] mdata, sximm8, sximm5;
+  reg [7:0] PC;
+  reg write, loada, loadb, asel, bsel, loadc, loads;
+  reg [1:0] vsel;
   reg [2:0] readnum, writenum;
   reg [1:0] shift, ALUop;
 
-  wire [15:0] datapath_out;
-  wire [2:0] status;
+  wire [15:0] out;
+  wire N, V, Z ;
 
   reg err;
 
@@ -27,19 +29,23 @@ module datapath_tb;
                 .ALUop       (ALUop),
                 .loadc       (loadc),
                 .loads       (loads),
+					 .sximm5      (sximm5),
 
                 // set when "writing back" to register file
                 .writenum    (writenum),
                 .write       (write),  
-                .datapath_in (datapath_in),
+                .mdata (mdata),
+					 .sximm8 (sximm8),
+					 .PC (PC),
 
                 // outputs
-                .Z_out       (Z_out),
-                .datapath_out(datapath_out)
+					 .N       (N),
+					 .V       (V),
+                .Z       (Z),
+                .out(out)
              );
 
-  // autograder needs to be able to access the contents of your register file
-  // using the following statements (wire used as follows acts like assign)
+  
   wire [15:0] R0 = DUT.REGFILE.R0;
   wire [15:0] R1 = DUT.REGFILE.R1;
   wire [15:0] R2 = DUT.REGFILE.R2;
@@ -66,7 +72,7 @@ module datapath_tb;
     err = 0;
     
 	 
-    datapath_in = 0;
+    sximm8 = 0; sximm5 = 0;
     write = 0; vsel=0; loada=0; loadb=0; asel=0; bsel=0; loadc=0; loads=0;
     readnum = 0; writenum=0;
     shift = 0; ALUop=0;
@@ -86,10 +92,10 @@ module datapath_tb;
 	 //MOV R7, #74
 
     // MOV R0, #25
-    datapath_in = 16'h25; 
+    sximm8 = 16'h25; 
     writenum = 3'd0;
     write = 1'b1;
-    vsel = 1'b1;
+    vsel = 2'b10;
     #10; // wait for clock 
 
     // the following checks if MOV was executed correctly
@@ -100,10 +106,10 @@ module datapath_tb;
 
 
     // MOV R1, #47
-    datapath_in = 16'h47;
+    sximm8 = 16'h47;
     writenum = 3'd1;
     write = 1'b1;
-    vsel = 1'b1;
+    vsel = 2'b10;
     #10; // wait for clock 
 
     // the following checks if MOV was executed correctly
@@ -113,10 +119,10 @@ module datapath_tb;
     end
 	 
 	 // MOV R2, #55
-    datapath_in = 16'h55; 
+    sximm8 = 16'h55; 
     writenum = 3'd2;
     write = 1'b1;
-    vsel = 1'b1;
+    vsel = 2'b10;
     #10; // wait for clock 
 
     // the following checks if MOV was executed correctly
@@ -126,10 +132,10 @@ module datapath_tb;
     end
 	 
 	 // MOV R3, #42
-    datapath_in = 16'h42; 
+    sximm8 = 16'h42; 
     writenum = 3'd3;
     write = 1'b1;
-    vsel = 1'b1;
+    vsel = 2'b10;
     #10; // wait for clock 
 
     // the following checks if MOV was executed correctly
@@ -139,10 +145,10 @@ module datapath_tb;
     end
 	 
 	 // MOV R4, #10
-    datapath_in = 16'h10; 
+    sximm8 = 16'h10; 
     writenum = 3'd4;
     write = 1'b1;
-    vsel = 1'b1;
+    vsel = 2'b10;
     #10; // wait for clock 
 
     // the following checks if MOV was executed correctly
@@ -152,10 +158,10 @@ module datapath_tb;
     end
 	 
 	 // MOV R5, #23
-    datapath_in = 16'h23; // h for hexadecimal
+    sximm8 = 16'h23; // h for hexadecimal
     writenum = 3'd5;
     write = 1'b1;
-    vsel = 1'b1;
+    vsel = 2'b10;
     #10; // wait for clock 
 
     // the following checks if MOV was executed correctly
@@ -165,10 +171,10 @@ module datapath_tb;
     end
 	 
 	 // MOV R6, #59
-    datapath_in = 16'h59; // h for hexadecimal
+    sximm8 = 16'h59; // h for hexadecimal
     writenum = 3'd6;
     write = 1'b1;
-    vsel = 1'b1;
+    vsel = 2'b10;
     #10; // wait for clock 
 
     // the following checks if MOV was executed correctly
@@ -178,10 +184,10 @@ module datapath_tb;
     end
 	 
 	 // MOV R7, #74
-    datapath_in = 16'h74; // h for hexadecimal
+    sximm8 = 16'h74; // h for hexadecimal
     writenum = 3'd7;
     write = 1'b1;
-    vsel = 1'b1;
+    vsel = 2'b10;
     #10; // wait for clock 
 
     // the following checks if MOV was executed correctly
@@ -214,15 +220,15 @@ module datapath_tb;
     bsel = 1'b0;
     ALUop = 2'b00;
     loadc = 1'b1;
-    loads = 1'b1;
+    loads = 1'b0;
     #10; // wait for clock
     loadc = 1'b0;
-    loads = 1'b0;
+    
 
     // step 4 - store contents of C into R2
     write = 1'b1;
     writenum = 3'd2;
-    vsel = 1'b0;
+    vsel = 2'b0;
     #10;
     write = 0;
 
@@ -232,22 +238,17 @@ module datapath_tb;
       $stop; 
     end
 
-    if (datapath_out !== 16'h65) begin 
+    if (out !== 16'h65) begin 
       err = 1; 
       $display("FAILED: ADD R2, R5, R3 -- datapath_out=%h is wrong, expected %h", R2, 16'h65); 
       $stop; 
     end
 
-    if (Z_out !== 1'b0) begin
-      err = 1; 
-      $display("FAILED: ADD R2, R5, R3 -- Z_out=%b is wrong, expected %b", Z_out, 1'b0); 
-      $stop; 
-    end
 	 
 	 
 	 ////////////////////////////////////////////////////////////
 
-    // SUB R1, R3, R0
+    // CMP R3, R0
     // step 1 - load contents of R0 into B reg
     readnum = 3'd0; 
     loadb = 1'b1;
@@ -260,41 +261,25 @@ module datapath_tb;
     #10; // wait for clock
     loada = 1'b0;
 
-    // step 3 - perform subraction of contents of A and B registers, load into C
+    // step 3 - perform cmp operation of contents of A and B registers, load status into S
     shift = 2'b00;
     asel = 1'b0;
     bsel = 1'b0;
     ALUop = 2'b01;
-    loadc = 1'b1;
+    loadc = 1'b0;
     loads = 1'b1;
     #10; // wait for clock
-    loadc = 1'b0;
     loads = 1'b0;
 
-    // step 4 - store contents of C into R1
-    write = 1'b1;
-    writenum = 3'd1;
-    vsel = 1'b0;
-    #10;
-    write = 0;
+ 
 
-    if (R1 !== 16'h1D) begin 
+    if ({N,V,Z} !== 3'b000) begin 
       err = 1; 
-      $display("FAILED: SUB R1, R3, R0 -- Regs[R1]=%h is wrong, expected %h", R1, 16'h1D); 
+      $display("FAILED: CMP R3, R0 -- status flags are wrong, expected N = %b, V = %b, Z = %b", 1'b0, 1'b0, 1'b0); 
        
     end
 
-    if (datapath_out !== 16'h1D) begin 
-      err = 1; 
-      $display("FAILED: SUB R1, R3, R0 -- datapath_out=%h is wrong, expected %h", R1, 16'h1D); 
-       
-    end
-
-    if (Z_out !== 1'b0) begin
-      err = 1; 
-      $display("FAILED: SUB R1, R3, R0 -- Z_out=%b is wrong, expected %b", Z_out, 1'b0); 
-       
-    end
+   
 	 
 	////////////////////////////////////////////////////////////
 
@@ -317,15 +302,14 @@ module datapath_tb;
     bsel = 1'b0;
     ALUop = 2'b10;
     loadc = 1'b1;
-    loads = 1'b1;
+    loads = 1'b0;
     #10; // wait for clock
     loadc = 1'b0;
-    loads = 1'b0;
 
     // step 4 - store contents of C into R4
     write = 1'b1;
     writenum = 3'd4;
-    vsel = 1'b0;
+    vsel = 2'b0;
     #10;
     write = 0;
 
@@ -335,17 +319,12 @@ module datapath_tb;
        
     end
 
-    if (datapath_out !== 16'h44) begin 
+    if (out !== 16'h44) begin 
       err = 1; 
       $display("FAILED: AND R4, R7, R5, LSL#1 -- datapath_out=%h is wrong, expected %h", R4, 16'h44); 
        
     end
 
-    if (Z_out !== 1'b0) begin
-      err = 1; 
-      $display("FAILED: AND R4, R7, R5, LSL#1 -- Z_out=%b is wrong, expected %b", Z_out, 1'b0); 
-      
-    end
 	 
 	////////////////////////////////////////////////////////////
 
@@ -353,6 +332,7 @@ module datapath_tb;
     // step 1 - load contents of R1 into B reg
     readnum = 3'd1; 
     loadb = 1'b1;
+	 loada = 1'b0;
     #10; // wait for clock
     loadb = 1'b0; // done loading B, set loadb to zero so don't overwrite A 
 
@@ -363,34 +343,28 @@ module datapath_tb;
     bsel = 1'b0;
     ALUop = 2'b11;
     loadc = 1'b1;
-    loads = 1'b1;
+	 loads = 1'b0;
+
     #10; // wait for clock
     loadc = 1'b0;
-    loads = 1'b0;
 
     // step 4 - store contents of C into R3
     write = 1'b1;
     writenum = 3'd3;
-    vsel = 1'b0;
+    vsel = 2'b0;
     #10;
     write = 0;
 
-    if (R3 !== 16'hFFE2) begin 
+    if (R3 !== 16'hFFB8) begin 
       err = 1; 
-      $display("FAILED: MVN R3, R1 -- Regs[R3]=%h is wrong, expected %h", R3, 16'hFFE2); 
+      $display("FAILED: MVN R3, R1 -- Regs[R3]=%h is wrong, expected %h", R3, 16'hFFB8); 
        
     end
 
-    if (datapath_out !== 16'hFFE2) begin 
+    if (out !== 16'hFFB8) begin 
       err = 1; 
-      $display("FAILED: MVN R3, R1-- datapath_out=%h is wrong, expected %h", R3, 16'hFFE2); 
+      $display("FAILED: MVN R3, R1-- datapath_out=%h is wrong, expected %h", R3, 16'hFFB8); 
        
-    end
-
-    if (Z_out !== 1'b0) begin
-      err = 1; 
-      $display("FAILED: MVN R3, R1 -- Z_out=%b is wrong, expected %b", Z_out, 1'b0); 
-      
     end
 	 
 
@@ -415,15 +389,14 @@ module datapath_tb;
     bsel = 1'b0;
     ALUop = 2'b00;
     loadc = 1'b1;
-    loads = 1'b1;
+    loads = 1'b0;
     #10; // wait for clock
     loadc = 1'b0;
-    loads = 1'b0;
 
     // step 4 - store contents of C into R2
     write = 1'b1;
     writenum = 3'd2;
-    vsel = 1'b0;
+    vsel = 2'b0;
     #10;
     write = 0;
 
@@ -433,18 +406,13 @@ module datapath_tb;
        
     end
 
-    if (datapath_out !== 16'hFFFF) begin 
+    if (out !== 16'hFFFF) begin 
       err = 1; 
       $display("FAILED: ADD R2, R3, R1 -- datapath_out=%h is wrong, expected %h", R2, 16'hFFFF); 
        
     end
 
-    if (Z_out !== 1'b0) begin
-      err = 1; 
-      $display("FAILED: ADD R2, R3, R1 -- Z_out=%b is wrong, expected %b", Z_out, 1'b0); 
-       
-    end
-	 
+    
 	 ////////////////////////////////////////////////////////////
 
     ////////////////////////////////////////////////////////////
@@ -453,6 +421,7 @@ module datapath_tb;
     // step 1 - load contents of R2 into B reg
     readnum = 3'd2; 
     loadb = 1'b1;
+	 loada = 1'b0;
     #10; // wait for clock
     loadb = 1'b0; // done loading B, set loadb to zero so don't overwrite A 
 
@@ -463,33 +432,61 @@ module datapath_tb;
     bsel = 1'b0;
     ALUop = 2'b11;
     loadc = 1'b1;
-    loads = 1'b1;
+    loads = 1'b0;
     #10; // wait for clock
     loadc = 1'b0;
-    loads = 1'b0;
+    
 
     // step 4 - store contents of C into R6
     write = 1'b1;
     writenum = 3'd6;
-    vsel = 1'b0;
+    vsel = 2'b0;
     #10;
     write = 0;
 
     if (R6 !== 16'h0) begin 
       err = 1; 
-      $display("FAILED: MVN R6, R2 -- Regs[R6]=%h is wrong, expected %h", R6, 16'h10); 
+      $display("FAILED: MVN R6, R2 -- Regs[R6]=%h is wrong, expected %h", R6, 16'h0); 
        
     end
 
-    if (datapath_out !== 16'h0) begin 
+    if (out !== 16'h0) begin 
       err = 1; 
       $display("FAILED: MVN R6, R2 -- datapath_out=%h is wrong, expected %h", R6, 16'h0); 
        
     end
+	 
+	 ////////////////////////////////////////////////////////////
 
-    if (Z_out !== 1'b1) begin
+    // CMP R6, R4
+    // step 1 - load contents of R3 into B reg
+    readnum = 3'd4; 
+    loadb = 1'b1;
+	 loada = 1'b0;
+    #10; // wait for clock
+    loadb = 1'b0; // done loading B, set loadb to zero so don't overwrite A 
+
+    // step 2 - load contents of R6 into A reg 
+    readnum = 3'd6; 
+    loada = 1'b1;
+    #10; // wait for clock
+    loada = 1'b0;
+
+    // step 3 - perform cmp operation of contents of A and B registers, load status into S
+    shift = 2'b00;
+    asel = 1'b0;
+    bsel = 1'b0;
+    ALUop = 2'b01;
+    loadc = 1'b0;
+    loads = 1'b1;
+    #10; // wait for clock
+    loads = 1'b0;
+
+ 
+
+    if ({N,V,Z} !== 3'b100) begin 
       err = 1; 
-      $display("FAILED: MVN R6, R2 -- Z_out=%b is wrong, expected %b", Z_out, 1'b1); 
+      $display("FAILED: CMP R3, R0 -- status flags are wrong, expected N = %b, V = %b, Z = %b", 1'b1, 1'b0, 1'b0); 
        
     end
 	 
@@ -500,6 +497,7 @@ module datapath_tb;
     // step 1 - load contents of R1 into B reg
     readnum = 3'd1; 
     loadb = 1'b1;
+	 loada = 1'b0;
     #10; // wait for clock
     loadb = 1'b0; // done loading B, set loadb to zero so don't overwrite A 
 
@@ -515,42 +513,36 @@ module datapath_tb;
     bsel = 1'b0;
     ALUop = 2'b00;
     loadc = 1'b1;
-    loads = 1'b1;
+    loads = 1'b0;
     #10; // wait for clock
     loadc = 1'b0;
-    loads = 1'b0;
 
     // step 4 - store contents of C into R5
     write = 1'b1;
     writenum = 3'd5;
-    vsel = 1'b0;
+    vsel = 2'b0;
     #10;
     write = 0;
 
-    if (R5 !== 16'h52) begin 
+    if (R5 !== 16'h67) begin 
       err = 1; 
-      $display("FAILED:  ADD R5, R4, R1, LSR#1 -- Regs[R4]=%h is wrong, expected %h", R5, 16'h52); 
+      $display("FAILED:  ADD R5, R4, R1, LSR#1 -- Regs[R4]=%h is wrong, expected %h", R5, 16'h67); 
        
     end
 
-    if (datapath_out !== 16'h52) begin 
+    if (out !== 16'h67) begin 
       err = 1; 
-      $display("FAILED:  ADD R5, R4, R1, LSR#1 -- datapath_out=%h is wrong, expected %h", R5, 16'h52); 
+      $display("FAILED:  ADD R5, R4, R1, LSR#1 -- datapath_out=%h is wrong, expected %h", R5, 16'h67); 
        
-    end
-
-    if (Z_out !== 1'b0) begin
-      err = 1; 
-      $display("FAILED:  ADD R5, R4, R1, LSR#1 -- Z_out=%b is wrong, expected %b", Z_out, 1'b0); 
-      
     end
 	 
 	 ////////////////////////////////////////////////////////////
 
-    // AND R1, R2, R3 (shift = 2'b11)
+    // AND R1, R2, R3, ASR #1
     // step 1 - load contents of R3 into B reg
     readnum = 3'd3; 
     loadb = 1'b1;
+	 loada = 1'b0;
     #10; // wait for clock
     loadb = 1'b0; // done loading B, set loadb to zero so don't overwrite A 
 
@@ -566,33 +558,26 @@ module datapath_tb;
     bsel = 1'b0;
     ALUop = 2'b10;
     loadc = 1'b1;
-    loads = 1'b1;
+    loads = 1'b0;
     #10; // wait for clock
     loadc = 1'b0;
-    loads = 1'b0;
 
     // step 4 - store contents of C into R1
     write = 1'b1;
     writenum = 3'd1;
-    vsel = 1'b0;
+    vsel = 2'b0;
     #10;
     write = 0;
 
-    if (R1 !== 16'hFFF1) begin 
+    if (R1 !== 16'hFFDC) begin 
       err = 1; 
-      $display("FAILED: AND R1, R2, R3 (shift = 2'b11) -- Regs[R4]=%h is wrong, expected %h", R1, 16'hFFF1); 
+      $display("FAILED: AND R1, R2, R3, ASR #1 -- Regs[R4]=%h is wrong, expected %h", R1, 16'hFFDC); 
       
     end
 
-    if (datapath_out !== 16'hFFF1) begin 
+    if (out !== 16'hFFDC) begin 
       err = 1; 
-      $display("FAILED: AND R1, R2, R3 (shift = 2'b11) -- datapath_out=%h is wrong, expected %h", R1, 16'hFFF1); 
-      
-    end
-
-    if (Z_out !== 1'b0) begin
-      err = 1; 
-      $display("FAILED: AND R1, R2, R3 (shift = 2'b11) -- Z_out=%b is wrong, expected %b", Z_out, 1'b0); 
+      $display("FAILED: AND R1, R2, R3, ASR #1 -- datapath_out=%h is wrong, expected %h", R1, 16'hFFDC); 
       
     end
 
@@ -602,6 +587,11 @@ module datapath_tb;
 		 #510;
 		 if (err === 0) begin
 			$display("PASSED: The datapath works as expected.");
+			$stop;
+		 end
+		 
+		 else begin
+			$display("FAILED: The datapath does not work as expected.");
 			$stop;
 		 end
 	end
